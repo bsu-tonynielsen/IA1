e2db719b1a43a77bd444c8c0c0bf907e8c7e3615
Tony Nielsen <tonynielsen@u.boisestate.edu>
2022-10-11 21:09:06 -0600

    Comments 3

--- ia1-main/NodeFactExpr.java
+++ ia1-main/NodeFactExpr.java
@@ -3,20 +3,34 @@
 // 10-11-2022
 
 /*
- * Currently unused
+ * Creates nodes for evaluation
  */
 public class NodeFactExpr extends NodeFact {
 
+	//instantiate variables
 	private NodeExpr expr;
 
+	/**
+	 * Main constructor
+	 * 
+	 * @param expr
+	 */
 	public NodeFactExpr(NodeExpr expr) {
 		this.expr=expr;
 	}
 
+	/**
+	 * @param env
+	 * @return - eval double
+	 * @throws EvalException
+	 */
 	public double eval(Environment env) throws EvalException {
 		return expr.eval(env);
 	}
 
+	/**
+	 * @return - translated C code
+	 */
 	public String code() { return "("+expr.code()+")"; }
 
 }
