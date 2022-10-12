b219c8e40a7f9d35e6188b3e26e7ae035b8388db
Tony Nielsen <tonynielsen@u.boisestate.edu>
2022-10-11 20:14:50 -0600

    lets go

---
+++ ia1/test/test-minus/gen.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+double x;
+x=0;x=x;
+x=1-2;printf("%.1lf\n",(double)(1-2));return 0;
+}
---
+++ ia1/test/test-two/gen.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+double x,y;
+x=0;x=x;
+x=1+2;printf("%.1lf\n",(double)(1+2));y=x+3;printf("%.1lf\n",(double)(x+3));return 0;
+}
